93d9a721b9c42763dbdc9281e48f1bdd58859b67
donneyo <donneyo63@gmail.com>
2024-08-19 04:17:29 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+            // Prompt the user to enter an integer
+            Console.Write("Please enter an integer: ");
+            int number = int.Parse(Console.ReadLine());
+
+            // Check if the number is even or odd
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+
+            // Keep the console window open
+            Console.ReadLine();
+    }
+}
